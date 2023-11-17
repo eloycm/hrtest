99082e8a64b8274d69929c733254dde611bc43af
eloycm <eloy@eloycortinez.com>
2023-11-17 12:29:19 -0500

    Invert Array

---
+++ hackerranktest/Reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerranktest
+{
+    public class Reverse
+    {
+        public static T[] ReverseArray<T>(T[] InputArray)
+        {
+            var myStack = new Stack<T>();
+            for (int i = 0; i < InputArray.Length; i++)
+            {
+                myStack.Push(InputArray[i]);
+            }
+            return myStack.ToArray();
+        }
+ 
+    }
+}
---
+++ hackerranktestTests/InvertArrayTest.cs
@@ -0,0 +1,37 @@
+﻿using hackerranktest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankApp.Tests
+{
+    [TestClass()]
+    public class InvertArrayTest
+    {
+        [TestMethod()]
+        public void StringReverseTest()
+        {
+            var s = "this is a test string";
+            var sArray = s.ToCharArray();
+            var invertedArray =Reverse.ReverseArray<char>(sArray);
+            var actual = new string(invertedArray);
+            var expected = "gnirts tset a si siht";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void WordReverseTest()
+        {
+            var s = "this is a test string";
+            var sArray = s.Split(' ');
+            var invertedArray = Reverse.ReverseArray<string>(sArray);
+            var actual = string.Join(" ", invertedArray);
+            var expected = "string test a is this";
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
