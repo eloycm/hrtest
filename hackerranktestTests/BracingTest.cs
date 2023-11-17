29444cbc55774d1b4ebfaf8cc830731168e43f19
eloycm <eloy@eloycortinez.com>
2023-11-16 21:51:33 -0500

    Refactoring version 3

--- hackerranktestTests/BracingTest.cs
+++ hackerranktestTests/BracingTest.cs
@@ -1,51 +1,16 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using hackerranktest;
+using HackerRankApp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using hackerranktest;
-namespace hackerranktest.Tests
+namespace HackerRankApp.Tests
 {
     [TestClass()]
     public class BracingTest
     {
-        [TestMethod()]
-        public void divisionTest()
-        {
-            var r = IntMath.division(4, 2);
-            Assert.AreEqual(2, r);
-
-        }
-        [TestMethod()]
-        public void divisionTest4()
-        {
-            var r = IntMath.division(300, 2);
-            Assert.AreEqual(150, r);
-
-        }
-        [TestMethod()]
-        public void divisionTest2()
-        {
-            var r = IntMath.division(100, 2);
-            Assert.AreEqual(50, r);
-
-        }
-        [TestMethod()]
-        public void divisionTest3()
-        {
-            var r = IntMath.division(5, 2);
-            Assert.AreEqual(2, r);
-
-        }
-        [TestMethod()]
-        public void divisionTest5()
-        {
-            var r = IntMath.division(6, 2);
-            Assert.AreEqual(3, r);
-
-        }
+        
         [TestMethod()]
         public void IsBracedTest()
         {
---
+++ hackerranktestTests/IntMathTest.cs
@@ -0,0 +1,50 @@
+﻿using HackerRankApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankApp.Tests
+{
+    [TestClass()]
+    public class IntMathTest
+    {
+        [TestMethod()]
+        public void divisionTest()
+        {
+            var r = IntMath.division(4, 2);
+            Assert.AreEqual(2, r);
+
+        }
+        [TestMethod()]
+        public void divisionTest4()
+        {
+            var r = IntMath.division(300, 2);
+            Assert.AreEqual(150, r);
+
+        }
+        [TestMethod()]
+        public void divisionTest2()
+        {
+            var r = IntMath.division(100, 2);
+            Assert.AreEqual(50, r);
+
+        }
+        [TestMethod()]
+        public void divisionTest3()
+        {
+            var r = IntMath.division(5, 2);
+            Assert.AreEqual(2, r);
+
+        }
+        [TestMethod()]
+        public void divisionTest5()
+        {
+            var r = IntMath.division(6, 2);
+            Assert.AreEqual(3, r);
+
+        }
+    }
+}
