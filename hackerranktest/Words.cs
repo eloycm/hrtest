8d1fe791ab4c77c9051aca82eb4ec4df128cb38a
eloycm <eloy@eloycortinez.com>
2023-11-25 16:17:26 -0500

    Top 3 words problem

---
+++ hackerranktest/Words.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerranktest
+{
+    public class Words
+    {
+        public static Dictionary<string,int> WordCounter(string inputString)
+        {
+            var rs= new Dictionary<string,int>();
+            var strings=inputString.Split(' ');
+            foreach (var s in strings)
+            {
+                if (string.IsNullOrWhiteSpace(s))
+                    continue;
+                if (rs.ContainsKey(s))
+                {
+                    rs[s] ++;
+                    continue;
+                }
+                rs.Add(s, 1 );
+            }
+            
+            return rs;
+        }
+        public static List<string> TopNWords(string inputString, int nWords)
+        {
+            
+            var orderWords = WordCounter(inputString).OrderByDescending(x => x.Value).ThenBy(x => x.Key).Select(x=>x.Key).Take(nWords).ToList();
+           
+            return orderWords;
+
+        }
+    }
+}
---
+++ hackerranktestTests/WordsTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hackerranktest;
+
+namespace hackerranktestTests
+{
+    [TestClass()]
+    public class WordsTest
+    {
+       
+        [TestMethod()]
+        public void TestTopNwords()
+        {
+            //var testString = "hola a todos hola a todas y a otros";
+            // var words = Words.TopNWords(testString, 1000);
+            var expected = new List<string> { "e", "ddd", "aa" };
+            var actual = Top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"); ;
+            CollectionAssert.AreEqual(expected, actual);
+
+            expected = new List<string> { "a", "of", "on" };
+            actual = Top3(string.Join("\n", new string[]{"In a village of La Mancha, the name of which I have no desire to call to",
+                  "mind, there lived not long since one of those gentlemen that keep a lance",
+                  "in the lance-rack, an old buckler, a lean hack, and a greyhound for",
+                  "coursing. An olla of rather more beef than mutton, a salad on most",
+                  "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra",
+                  "on Sundays, made away with three-quarters of his income." }));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        public List<string> Top3(string inputString)
+        {
+            var rs = Words.TopNWords(inputString, 3);
+            return rs;
+        }
+    }
+    
+}
