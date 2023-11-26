97557800b0329c38f707c813ed1b1db5d1f9f7a0
eloycm <eloy@eloycortinez.com>
2023-11-25 19:56:27 -0500

    google facilities distance problem

---
+++ hackerranktest/Distance/ExploringList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerranktest.Distance
+{
+    public class ExploringList
+    {
+        protected HashSet<string> FoundFacilities = new HashSet<string>();
+        protected HashSet<string> UniqueDesideredItems = new HashSet<string>();
+        public bool AllFound { get; set; } = false;
+        public List<string> DesideredItems { set
+            {
+                UniqueDesideredItems.UnionWith(value);
+            } }
+
+        public void AddBlock(List<string> block)
+        {
+            var foundItems = UniqueDesideredItems.Intersect(block);
+            FoundFacilities.UnionWith(foundItems);
+            AllFound = FoundFacilities.Count == UniqueDesideredItems.Count;
+        }   
+            
+    }
+}
---
+++ hackerranktest/Distance/GDistance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerranktest.Distance
+{
+    public class GDistance
+    {
+        public int MaxDistanceTo(List<string> myFacilities, List<string>[] Blocks, int myPosition)
+        {
+            var exploring = new ExploringList();
+      
+            exploring.DesideredItems = myFacilities.ToList();
+
+            for (int i = myPosition; i < Blocks.Length; i++)
+            {
+                //up
+                var block = Blocks[i];
+                exploring.AddBlock(block);
+                if (exploring.AllFound)
+                    return i - myPosition;
+
+                //down
+                if (myPosition - i <= 0)
+                    continue;
+                block = Blocks[myPosition - i];
+                exploring.AddBlock(block);
+                if (exploring.AllFound)
+                    return myPosition -i;
+            }
+            return int.MaxValue;
+        }
+        public int FindBlock(List<string> myFacilities, List<string>[] Blocks)
+        {
+            var minDistance = int.MaxValue;
+            var rs = int.MaxValue;
+            for (int i = 0; i < Blocks.Length; i++)
+            {
+                var distance = MaxDistanceTo(myFacilities, Blocks,i);
+                if (minDistance > distance)
+                {
+                    minDistance = distance;
+                    rs= i;
+                }
+                    
+            }
+
+            return rs;
+
+        }
+
+    }
+}
